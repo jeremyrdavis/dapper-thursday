060600883c79babead3b512da467df8ba7f7a1b1
Jeremy Davis <jeremyrdavis@mac.com>
2023-02-15 10:09:39 -0500

    not puking

--- src/main/java/io/arrogantprogrammer/GreetingResource.java
+++ src/main/java/io/arrogantprogrammer/GreetingResource.java
@@ -1,16 +1,28 @@
 package io.arrogantprogrammer;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("/hello")
 public class GreetingResource {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GreetingResource.class);
+
+    @Inject
+    DaprService daprService;
+
+    String greeting = "Hello RESTEasy";
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        daprService.sendMessage(new DaprMessage("pubsub", "greetings", greeting));
+        return greeting;
     }
 }
